63ece3fd37223ff1771437caa887eff85e8e58b1
Joong <augustok87@gmail.com>
2021-01-06 18:59:06 -0300

    removed an import from nav.js

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'gatsby';
 import styled from 'styled-components';
 import Logo from './Logo';
-import { clearError } from '../../.cache/error-overlay-handler';
 
 const NavStyles = styled.nav`
   /* margin-bottom: 3rem; */
